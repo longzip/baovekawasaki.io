3706c428685d012f60e1bac7d601d2a4f9a34b2d
Lo Van Long <longlv@v-auto.com.vn>
2021-06-26 06:39:58 +0700

    generate

--- components/Footer.vue
+++ components/Footer.vue
@@ -68,6 +68,24 @@
             /></a>
             <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"></script>
           </p>
+          <div
+            class="fixed bottom-24 left-5 flex text-white bg-red-600 hover:bg-red-700 rounded-lg p-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 flex-initial"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+              /></svg
+            ><a class="flex-initial text-lg" href="tel:0978333963">097 833 39 63</a>
+          </div>
         </div>
         <div class="w-full md:w-6/12 px-4">
           <div class="flex flex-wrap items-top mb-6">
