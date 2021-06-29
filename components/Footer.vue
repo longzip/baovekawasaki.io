cb1afbb46e276e3a9fdc0b6e718b869158dfc3b9
Lo Van Long <longlv@v-auto.com.vn>
2021-06-29 10:52:25 +0700

    generate

--- components/Footer.vue
+++ components/Footer.vue
@@ -190,19 +190,7 @@
         ></path>
       </svg>
     </div>
-    <script id="mcjs">
-      !(function (c, h, i, m, p) {
-        ;(m = c.createElement(h)),
-          (p = c.getElementsByTagName(h)[0]),
-          (m.async = 1),
-          (m.src = i),
-          p.parentNode.insertBefore(m, p)
-      })(
-        document,
-        'script',
-        'https://chimpstatic.com/mcjs-connected/js/users/dafd0f159bea8e5be932061e6/3b5f602fcb454a5cdfb96f510.js'
-      )
-    </script>
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/dafd0f159bea8e5be932061e6/3b5f602fcb454a5cdfb96f510.js");</script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script
       async
---
+++ docs/static/1624938741/tuyen-dung/tuyen-bao-ve-chung-cu-tai-ha-noi-viec-lam-bao-ve-bao-an-o/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fbaovekawasaki.tk\u002Fstatic\u002F1624938741",layout:"default",error:null,serverRendered:true,routePath:"\u002Ftuyen-dung\u002Ftuyen-bao-ve-chung-cu-tai-ha-noi-viec-lam-bao-ve-bao-an-o",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fbaovekawasaki.tk\u002F"}}}}("\u002F"));
